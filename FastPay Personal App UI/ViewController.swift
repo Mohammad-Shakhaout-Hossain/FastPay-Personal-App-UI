f4ee9944e8b5ca07c679b67a2079c74c2fe5c8fa
shakhawat hossain <shakhwt@gmail.com>
2020-12-24 13:11:14 +0600

    Initial Commit

---
+++ FastPay Personal App UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastPay Personal App UI
+//
+//  Created by Shakhawat Hossain Shakib on 24/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
