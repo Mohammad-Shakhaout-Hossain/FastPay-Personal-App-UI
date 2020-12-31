5e49620c5809528345168cbe2be86fd0c837c5f1
shakhawat hossain <shakhwt@gmail.com>
2020-12-31 12:47:21 +0600

    code structure fixes

--- FastPay Personal App UI/HomeViewController.swift
+++ FastPay Personal App UI/HomeViewController.swift
@@ -9,38 +9,38 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    let transition = SlideinTransition()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.navigationItem.titleView = UIImageView(image: UIImage(named: "Group 13114"))
-    }
+let transition = SlideinTransition()
+override func viewDidLoad() {
+    super.viewDidLoad()
+    self.navigationItem.titleView = UIImageView(image: UIImage(named: "Group 13114"))
+}
 
 
-    @IBAction func menuTapped(_ sender: UIBarButtonItem){
-       guard let menuViewController = storyboard?.instantiateViewController(withIdentifier: "menuViewController") as? MenuViewController else { return }
-        menuViewController.didTapMenuType = { menuType in
-            self.transiitionToNew(menuType: menuType)
-        }
-        menuViewController.modalPresentationStyle = .overCurrentContext
-        menuViewController.transitioningDelegate = self
-        present(menuViewController, animated: true)
-        
+@IBAction func menuTapped(_ sender: UIBarButtonItem){
+   guard let menuViewController = storyboard?.instantiateViewController(withIdentifier: "menuViewController") as? MenuViewController else { return }
+    menuViewController.didTapMenuType = { menuType in
+        self.transiitionToNew(menuType: menuType)
     }
-    
-    func transiitionToNew (menuType: MenuType) {
-        let title = String(describing: menuType).capitalized
-        self.title = title
-    }
+    menuViewController.modalPresentationStyle = .overCurrentContext
+    menuViewController.transitioningDelegate = self
+    present(menuViewController, animated: true)
     
 }
 
+func transiitionToNew (menuType: MenuType) {
+    let title = String(describing: menuType).capitalized
+    self.title = title
+}
+
+}
+
 extension HomeViewController: UIViewControllerTransitioningDelegate {
-    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        transition.isPresenting = true
-        return transition
-    }
-    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        transition.isPresenting = false
-        return transition
-    }
+func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    transition.isPresenting = true
+    return transition
+}
+func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    transition.isPresenting = false
+    return transition
+}
 }
--- FastPay Personal App UI/MenuViewController.swift
+++ FastPay Personal App UI/MenuViewController.swift
@@ -8,36 +8,35 @@
 import UIKit
 
 enum MenuType: Int {
-    case accountSettings
-    case transactionHistry
-    case limits
-    case referAFriend
-    case promotions
-    case appSettings
-    case supportHelp
-    case logOut
+case accountSettings
+case transactionHistry
+case limits
+case referAFriend
+case promotions
+case appSettings
+case supportHelp
+case logOut
 }
 
 class MenuViewController: UITableViewController {
-    
-    var didTapMenuType: ((MenuType) -> Void)?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+var didTapMenuType: ((MenuType) -> Void)?
 
-        // Do any additional setup after loading the view.
-    }
+override func viewDidLoad() {
+    super.viewDidLoad()
 
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let menuType = MenuType(rawValue: indexPath.row) else {return}
-        dismiss(animated: true) { [weak self] in
-            print("Dismission: \(menuType)")
-            self?.didTapMenuType?(menuType)
-        }
+}
+
+override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    guard let menuType = MenuType(rawValue: indexPath.row) else {return}
+    dismiss(animated: true) { [weak self] in
+        print("Dismission: \(menuType)")
+        self?.didTapMenuType?(menuType)
     }
-    @IBAction func dismissBtnTapped(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    
+}
+@IBAction func dismissBtnTapped(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
+}
+
+
 }
